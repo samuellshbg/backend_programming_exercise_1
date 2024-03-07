7856e225806e605d8838483ade4ce1820ebcca3c
SAMUEL <samuelsihombing526@gmail.com>
2024-03-07 16:43:05 +0700

    535230197 Samuel George Sihombing

---
+++ soal_1.js
@@ -0,0 +1,23 @@
+function isPrime(n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    if (n % 2 === 0 || n % 3 === 0) return false;
+
+    for (let i = 5; i * i <= n; i += 6) {
+        if (n % i === 0 || n % (i + 2) === 0) return false;
+    }
+
+    return true;
+}
+
+function displayPrimes() {
+    for (let num = 1; num <= 1000; num++) {
+        if (isPrime(num)) {
+            console.log(num);
+        }
+    }
+}
+
+displayPrimes();
+
